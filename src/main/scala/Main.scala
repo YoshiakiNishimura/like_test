093599c4253f7b8aafb8670012f1f3d7e5c02737
Yoshiaki Nishimura <nishimura@nautilus-technologies.com>
2025-06-02 15:24:09 +0900

    add setting

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,63 @@
-@main def hello(): Unit =
-  println("Hello world!")
-  println(msg)
+import com.tsurugidb.iceaxe.TsurugiConnector
+import com.tsurugidb.iceaxe.metadata.TgTableMetadata
+import com.tsurugidb.iceaxe.session.TsurugiSession
+import com.tsurugidb.iceaxe.sql.{TsurugiSqlStatement, TsurugiSqlQuery}
+import com.tsurugidb.iceaxe.sql.result.{
+  TsurugiResultEntity,
+  TsurugiStatementResult,
+  TsurugiQueryResult
+}
+import com.tsurugidb.iceaxe.transaction.manager.{
+  TgTmSetting,
+  TsurugiTransactionManager
+}
+import com.tsurugidb.iceaxe.transaction.option
+import com.tsurugidb.iceaxe.transaction.option.TgTxOption
+import com.tsurugidb.iceaxe.transaction.option.TgTxOptionLtx
+import com.tsurugidb.iceaxe.transaction.exception.TsurugiTransactionException
+import com.tsurugidb.iceaxe.transaction.TsurugiTransaction
 
-def msg = "I was compiled by Scala 3. :)"
+import com.tsurugidb.tsubakuro.common.{Session, SessionBuilder}
+import com.tsurugidb.tsubakuro.sql.{SqlClient, Transaction}
+import com.tsurugidb.tsubakuro.kvs.{KvsClient, RecordBuffer, TransactionHandle}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.{ExecutionContext}
+import scala.jdk.CollectionConverters._
+import scala.util.{Using, Try, Success, Failure}
+import java.net.URI
+
+private val Connect = "ipc://tsurugi"
+private val TableName = "like_table"
+private val ColumnString = "(c0 int, c1 varchar)"
+private val inputString = "abcdefghijklmnop"
+private val Columncount = 10
+
+
+@main def run(): Unit = {
+  println("start")
+  val endpoint = URI.create(Connect)
+  val connector = TsurugiConnector.of(endpoint)
+  val drop = s"DROP TABLE IF EXISTS ${TableName}"
+  val create = s"CREATE TABLE ${TableName} ${ColumnString}"
+  val session = SessionBuilder.connect(endpoint).create()
+  val sql = SqlClient.attach(session)
+  val kvs = KvsClient.attach(session)
+  println(s"drop and create table: ${TableName}")
+  Try {
+    val transaction = sql.createTransaction().await
+    transaction.executeStatement(drop).await
+    transaction.commit().await
+    transaction.close()
+  } recover { case e: Exception =>
+    println(e.getMessage)
+  }
+  Try {
+    val transaction = sql.createTransaction().await
+    transaction.executeStatement(create).await
+    transaction.commit().await
+    transaction.close()
+  } recover { case e: Exception =>
+    println(e.getMessage)
+  }
+}
